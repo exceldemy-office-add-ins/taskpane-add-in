e82f665d71394adaf79b4a4049e0827162b21dd8
Al Arafat Siddique <arafatdipto@gmail.com>
2022-08-16 16:36:04 +0600

    Project Setup
    - cleaned the project
    - Bootstrap CDN added in the taskpane.html page
    - checked if the user's version of Office supports all the Office.js APIs

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -7,30 +7,11 @@
 
 Office.onReady((info) => {
   if (info.host === Office.HostType.Excel) {
-    document.getElementById("sideload-msg").style.display = "none";
-    document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
+    // Determine if the user's version of Office supports all the Office.js APIs that are used in the tutorial.
+if (!Office.context.requirements.isSetSupported('ExcelApi', '1.7')) {
+  console.log('Sorry. The tutorial add-in uses Excel.js APIs that are not available in your version of Office.');
+}
+    
   }
 });
 
-export async function run() {
-  try {
-    await Excel.run(async (context) => {
-      /**
-       * Insert your Excel code here
-       */
-      const range = context.workbook.getSelectedRange();
-
-      // Read the range address
-      range.load("address");
-
-      // Update the fill color
-      range.format.fill.color = "yellow";
-
-      await context.sync();
-      console.log(`The range address was ${range.address}.`);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}
