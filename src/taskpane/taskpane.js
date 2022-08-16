c3ba6fc1c1b8c5016ba99a5187e4cf02bec10cb5
Al Arafat Siddique <arafatdipto@gmail.com>
2022-08-16 17:12:01 +0600

    Sample Table added
    export async function createTable() {
      try {
        await Excel.run(async (context) => {
    
          await context.sync();
        });
      } catch (error) {
        console.error(error);
        if(error instanceof OfficeExtension.Error){
          console.log("Debug info: " + JSON.stringify(error.debugInfo));
        }
      }
    }

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -11,7 +11,7 @@ Office.onReady((info) => {
 if (!Office.context.requirements.isSetSupported('ExcelApi', '1.7')) {
   console.log('Sorry. The tutorial add-in uses Excel.js APIs that are not available in your version of Office.');
 }
-    
+    document.getElementById("create-table").onclick = createTable;
   }
 });
 
@@ -23,3 +23,35 @@ document.addEventListener("DOMContentLoaded", (e)=>{
     splash.classList.add("display-none");
   },2000);
 })
+
+export async function createTable() {
+  try {
+    await Excel.run(async (context) => {
+      var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+      var expensesTable= currentWorksheet.tables.add("A1:D1", true);
+      expensesTable.name= "ExpensesTable";
+  
+      expensesTable.getHeaderRowRange().values = [["Date","Merchant", "Category", "Amount"]];
+      expensesTable.rows.add(null /*add at the end*/, [
+        ["1/1/2017", "The Phone Company GP", "Communications", "120"],
+        ["1/2/2017", "Northwind Electric Cars", "Transportation", "142.33"],
+        ["1/5/2017", "Best For You Organics Company", "Groceries", "27.9"],
+        ["1/10/2017", "Coho Vineyard", "Restaurant", "33"],
+        ["1/11/2017", "Bellows College", "Education", "350.1"],
+        ["1/15/2017", "Trey Research", "Other", "135"],
+        ["1/15/2017", "Best For You Organics Company", "Groceries", "97.88"]
+      ]);
+   
+      expensesTable.columns.getItemAt(3).getRange().numberFormat = [['\u20AC#,##0.00']];
+      expensesTable.getRange().format.autofitColumns();
+      expensesTable.getRange().format.autofitRows();
+ 
+      await context.sync();
+    });
+  } catch (error) {
+    console.error(error);
+    if(error instanceof OfficeExtension.Error){
+      console.log("Debug info: " + JSON.stringify(error.debugInfo));
+    }
+  }
+}
