bc9b1e70bf85c477ce0402fd9b368d75686e7867
Al Arafat Siddique <arafatdipto@gmail.com>
2022-08-21 12:59:36 +0600

    Sample Nav Bar Added
    A sample navbar added to taskpane3.html page.

--- src/taskpane/taskpane3/taskpane3.js
+++ src/taskpane/taskpane3/taskpane3.js
@@ -17,13 +17,13 @@ Office.onReady((info) => {
       document.getElementById("create-table").onclick = createTable;
     }
   });
-  
+  //Js Components Imports
   import {createTable} from './js_components/createTable'
   import {detectBlankCell} from './js_components/detectBlankCell'
   import { emptyCell } from './js_components/emptyCell';
   import { entireRow } from './js_components/entireRow';
 
-
+//HTML Comoponents Imports
 import { create_table } from './html_components/create_table';
 import { detect_blank_cell } from './html_components/detect_blank_cell';
 import { empty_cell } from './html_components/empty_cell';
@@ -31,10 +31,30 @@ import { entire_row } from './html_components/enitre_row';
   
   
   
-  // js for splash screen
+// js for splash screen
   const splash = document.querySelector(".splash");
   document.addEventListener("DOMContentLoaded", (e)=>{
     setTimeout(()=>{
       splash.classList.add("display-none");
     },2000);
   })
+
+
+// Sample Nav Bar 
+
+  $('.btn_menu').click(function(){
+    $(this).toggleClass("click");
+    $('.sidebar').toggleClass("show");
+  });
+    $('.feat-btn').click(function(){
+      $('nav ul .feat-show').toggleClass("show");
+      $('nav ul .first').toggleClass("rotate");
+    });
+    $('.serv-btn').click(function(){
+      $('nav ul .serv-show').toggleClass("show1");
+      $('nav ul .second').toggleClass("rotate");
+    });
+    $('nav ul li').click(function(){
+      $(this).addClass("active").siblings().removeClass("active");
+    });
+
